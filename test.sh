10093548de3c524df749f9e93b9b1bc207f41a2b
Erik Bryant <erikbryantology@gmail.com>
2013-02-07 23:06:04 -0800

    Using the lame makefile

--- test.sh
+++ test.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-CMD="g++ bigint.c lib.c test.c"
-echo $CMD
-$CMD
-if [[ $? -eq 0 ]]
+make
+if [[ $? -ne 0 ]]
 then
-  ./a.out
-  if [[ $? -ne 0 ]]
-  then
-    exit
-  fi
-else
   exit
 fi
 
