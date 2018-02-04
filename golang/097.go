17b9fda3499fec3d3c3c65d1c0f3727332ffe42e
Erik Bryant <erik_bryant@yahoo.com>
2018-02-03 20:00:32 -0800

    Solution for Project Euler #097.

---
+++ golang/097.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func mul(s string, n int) string {
+	val, _ := strconv.Atoi(s)
+	val *= n
+
+	// We only care about the last ten digits
+	str := strconv.Itoa(val)
+	if len(str) > 10 {
+		str = str[len(str)-10:]
+	}
+	return str
+}
+
+func main() {
+	product := "1"
+	for i := 1; i <= 7830457; i++ {
+		product = mul(product, 2)
+	}
+	p, _ := strconv.Atoi(product)
+	p *= 28433
+	p += 1
+	fmt.Println(p)
+}
