934651ae7eb47fb2d90ecd08a145531ea4cc9ac9
Erik Bryant <erik_bryant@yahoo.com>
2018-02-03 22:56:32 -0800

    Add a packed representation that should be faster for some operations.

--- golang/primes/primes.go
+++ golang/primes/primes.go
@@ -6,15 +6,37 @@ import (
 )
 
 const (
-	MAX_PRIME = 1000 * 1000
+	MAX_PRIME = 1000 * 1000 * 10
 )
 
 var (
-	Primes [MAX_PRIME + 1]bool
+	Primes          [MAX_PRIME + 1]bool
+	PackedPrimes    [MAX_PRIME + 1]int
+	packedPrimesLen int
 )
 
 func Prime(number int) bool {
 	return Primes[number]
+}
+
+func PackPrimes() {
+	j := 0
+	for i := 0; i < len(Primes); i++ {
+		if Prime(i) {
+			PackedPrimes[j] = i
+			j++
+		}
+	}
+	packedPrimesLen = j
+}
+
+func PackedIndex(n int) int {
+	for i := packedPrimesLen - 1; i >= 0; i-- {
+		if n >= PackedPrimes[i] {
+			return i
+		}
+	}
+	return 0
 }
 
 func excludes(upper int, c chan int) {
