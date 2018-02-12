beb61b38abcb8497660081a46712fb93224c03fe
Erik Bryant <erik_bryant@yahoo.com>
2018-02-11 16:41:12 -0800

    Solution for Project Euler #357.

---
+++ golang/357.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"./primes"
+	"fmt"
+	"math"
+)
+
+func primeDivisors(n int) bool {
+	//
+	// For d=1 and d=n the equation has the same result:
+	//   d=1 -> 1+n/1 = n+1
+	//   d=n -> n+n/n = n+1
+	// The same holds true for d=2 and d=(n/2). So, we
+	// only need to examine the first half of the divisors,
+	// since the second half has no new information. This
+	// equates to the sqrt(n).
+	//
+	root := int(math.Sqrt(float64(n)))
+	for d := 1; d <= root; d++ {
+		if n%d == 0 {
+			if !primes.Primes[d+n/d] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	primes.Init()
+	fmt.Println("Welcome to 357")
+
+	sum := 0
+	for n := 1; n <= 1000*1000*100; n++ {
+		if n%1000000 == 0 {
+			fmt.Println(n)
+		}
+		if primeDivisors(n) {
+			sum += n
+		}
+	}
+
+	fmt.Println("Sum: ", sum)
+}
