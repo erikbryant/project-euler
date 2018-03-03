a959a9b3e87c7aec594f0c037b665ab4d0f168ef
Erik Bryant <erik_bryant@yahoo.com>
2018-03-02 22:19:05 -0800

    Solution for Project Euler #347.

---
+++ golang/347/347.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"../primes"
+	"fmt"
+)
+
+func init() {
+	primes.Load("../primes.gob")
+}
+
+func doesItFactor(p, q, N int) bool {
+	if N%p != 0 || N%q != 0 {
+		return false
+	}
+
+	for N%p == 0 {
+		N = N / p
+	}
+
+	for N%q == 0 {
+		N = N / q
+	}
+
+	return N == 1
+}
+
+// For two distinct primes p and q let M(p,q,N) be the
+// largest positive integer ≤N only divisible by both
+// p and q and M(p,q,N)=0 if such a positive integer
+// does not exist.
+func M(p, q, N int) int {
+	max := p * q
+
+	if max > N {
+		return 0
+	}
+
+	pProduct := p
+	for pProduct*q <= N {
+		pqProduct := pProduct * q
+		for pqProduct <= N {
+			if pqProduct > max {
+				max = pqProduct
+			}
+			pqProduct = pqProduct * q
+		}
+		pProduct = pProduct * p
+	}
+
+	return max
+}
+
+// Let S(N) be the sum of all distinct M(p,q,N).
+func S(N int) (sum int) {
+	for pIndex := 0; primes.PackedPrimes[pIndex] < N; pIndex++ {
+		for qIndex := pIndex + 1; primes.PackedPrimes[qIndex]*primes.PackedPrimes[pIndex] <= N; qIndex++ {
+			sum += M(primes.PackedPrimes[pIndex], primes.PackedPrimes[qIndex], N)
+		}
+	}
+
+	return
+}
+
+func main() {
+	N := 1000 * 1000 * 10
+	sum := S(N)
+	fmt.Printf("S(%d) = %d\n", N, sum)
+}
---
+++ golang/347/347_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDoesItFActor(t *testing.T) {
+	testCases := []struct {
+		p        int
+		q        int
+		N        int
+		expected bool
+	}{
+		{2, 3, 96, true},
+		{3, 5, 75, true},
+		{3, 5, 90, false},
+	}
+
+	for _, testCase := range testCases {
+		answer := doesItFactor(testCase.p, testCase.q, testCase.N)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d, %d, %d expected %t, got %t", testCase.p, testCase.q, testCase.N, testCase.expected, answer)
+		}
+	}
+}
+
+func TestM(t *testing.T) {
+	testCases := []struct {
+		p        int
+		q        int
+		N        int
+		expected int
+	}{
+		{2, 3, 100, 96},
+		{3, 5, 100, 75},
+		{2, 73, 100, 0},
+		{5, 73, 100, 0},
+	}
+
+	for _, testCase := range testCases {
+		answer := M(testCase.p, testCase.q, testCase.N)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d, %d, %d expected %d, got %d", testCase.p, testCase.q, testCase.N, testCase.expected, answer)
+		}
+	}
+}
+
+func TestS(t *testing.T) {
+	testCases := []struct {
+		N        int
+		expected int
+	}{
+		{5, 0},
+		{6, 6},
+		{10, 16},
+		{100, 2262},
+		{1000, 193408},
+	}
+
+	for _, testCase := range testCases {
+		answer := S(testCase.N)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.N, testCase.expected, answer)
+		}
+	}
+}
