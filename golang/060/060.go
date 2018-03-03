23019f4a4774a002de7b07873fd7c653ba804558
Erik Bryant <erik_bryant@yahoo.com>
2018-03-03 14:36:24 -0800

    Solution to Project Euler #60.

---
+++ golang/060/060.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"../primes"
+	"fmt"
+	"math"
+	"strconv"
+)
+
+var (
+	powPrimes []int
+	maxPrime  int
+)
+
+func init() {
+	primes.Load("../primes.gob")
+
+	for i := 0; i < len(primes.PackedPrimes); i++ {
+		s := strconv.Itoa(primes.PackedPrimes[i])
+		powPrimes = append(powPrimes, int(math.Pow10(len(s))))
+	}
+
+	maxPrime = primes.PackedPrimes[len(primes.PackedPrimes)-1]
+}
+
+func allCombosPrime(p []int) bool {
+	for i := 0; i < len(p); i++ {
+		for j := i + 1; j < len(p); j++ {
+			p1 := primes.PackedPrimes[p[i]]
+			p2 := primes.PackedPrimes[p[j]]
+			p1Len := powPrimes[p[i]]
+			p2Len := powPrimes[p[j]]
+			c1 := p1*p2Len + p2
+			c2 := p2*p1Len + p1
+			if c1 > maxPrime || c2 > maxPrime {
+				return false
+			}
+			if !(primes.Prime(c1) && primes.Prime(c2)) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func printPrimes(p []int) int {
+	sum := 0
+
+	for i := 0; i < len(p); i++ {
+		sum += primes.PackedPrimes[p[i]]
+	}
+
+	if len(p) == 4 {
+		fmt.Println("Sum:", sum, p, []int{primes.PackedPrimes[p[0]], primes.PackedPrimes[p[1]], primes.PackedPrimes[p[2]], primes.PackedPrimes[p[3]]})
+	}
+
+	if len(p) == 5 {
+		fmt.Println("Sum:", sum, p, []int{primes.PackedPrimes[p[0]], primes.PackedPrimes[p[1]], primes.PackedPrimes[p[2]], primes.PackedPrimes[p[3]], primes.PackedPrimes[p[4]]})
+	}
+
+	return sum
+}
+
+func findPrimes(max int) {
+	for a := 0; primes.PackedPrimes[a] <= max; a++ {
+		for b := a + 1; primes.PackedPrimes[b] <= max; b++ {
+			if allCombosPrime([]int{a, b}) {
+				for c := b + 1; primes.PackedPrimes[c] <= max; c++ {
+					if allCombosPrime([]int{a, b, c}) {
+						for d := c + 1; primes.PackedPrimes[d] <= max; d++ {
+							if allCombosPrime([]int{a, b, c, d}) {
+								for e := 1; primes.PackedPrimes[e] <= max; e++ {
+									p := []int{a, b, c, d, e}
+									if allCombosPrime(p) {
+										printPrimes(p)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	findPrimes(10000)
+}
---
+++ golang/060/060_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"../primes"
+	"testing"
+)
+
+func TestAllCombosPrime(t *testing.T) {
+	testCases := []struct {
+		primeIndexes []int
+		primes       []int
+		expected     bool
+	}{
+		{[]int{0, 1, 2, 3}, []int{2, 3, 5, 7}, false},
+		{[]int{1, 3, 28, 121}, []int{3, 7, 109, 673}, true},
+	}
+
+	// allCombosPrime() takes the index of the primes, not the
+	// primes themselves. So, we have to pass it the indexes.
+	// Make sure we got the indexes right. :-)
+	for _, testCase := range testCases {
+		for i := 0; i < len(testCase.primeIndexes); i++ {
+			if testCase.primes[i] != primes.PackedPrimes[testCase.primeIndexes[i]] {
+				t.Errorf("ERROR: For %v, %v expected %d, got %d", testCase.primeIndexes, testCase.primes, testCase.primes[i], primes.PackedPrimes[testCase.primeIndexes[i]])
+			}
+		}
+	}
+
+	for _, testCase := range testCases {
+		answer := allCombosPrime(testCase.primeIndexes)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %v expected %t, got %t", testCase.primes, testCase.expected, answer)
+		}
+	}
+}
