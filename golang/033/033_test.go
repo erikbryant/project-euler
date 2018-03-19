739479b1461c02e920210d5e847f823b1e161890
Erik Bryant <erik_bryant@yahoo.com>
2018-03-18 18:05:53 -0700

    Add a test for reduce().

--- golang/033/033_test.go
+++ golang/033/033_test.go
@@ -21,3 +21,26 @@ func TestCancels(t *testing.T) {
 		}
 	}
 }
+
+func TestReduce(t *testing.T) {
+	testCases := []struct {
+		n         int
+		d         int
+		expectedN int
+		expectedD int
+	}{
+		{1, 2, 1, 2},
+		{2, 4, 1, 2},
+		{3, 9, 1, 3},
+		{4, 16, 1, 4},
+		{5, 100, 1, 20},
+		{4, 6, 2, 3},
+	}
+
+	for _, testCase := range testCases {
+		answer := reduce([]int{testCase.n, testCase.d})
+		if answer[0] != testCase.expectedN || answer[1] != testCase.expectedD {
+			t.Errorf("ERROR: For %d/%d expected %d/%d, got %d/%d", testCase.n, testCase.d, testCase.expectedN, testCase.expectedD, answer[0], answer[1])
+		}
+	}
+}
