6fbb99ce13d3eb907697374ddebbed9909870098
Erik Bryant <erik_bryant@yahoo.com>
2018-03-22 08:53:36 -0700

    Solution for Project Euler #99.

---
+++ golang/099/099.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
+
+// compare() determines which value is bigger.
+// Returns -1 if b1,e1 is bigger, 0 if they are equal, and 1 if b2,e2 is bigger
+func compare(b1 float64, e1 int, b2 float64, e2 int) (int, float64, int) {
+	// fmt.Printf("%.2f^%d <=> %.2f^%d\n", b1, e1, b2, e2)
+	if e1 < 2 && e2 < 2 {
+		answer1 := math.Pow(b1, float64(e1))
+		answer2 := math.Pow(b2, float64(e2))
+
+		if answer1 > answer2 {
+			return -1, b1, e1
+		}
+		if answer1 == answer2 {
+			return 0, b1, e1
+		}
+		return 1, b2, e2
+	}
+
+	if b1 == b2 {
+		if e1 == e2 {
+			return 0, b1, e1
+		}
+		if e1 > e2 {
+			return -1, b1, e1
+		}
+		return 1, b2, e2
+	}
+
+	if e1 == e2 {
+		if b1 == b2 {
+			return 0, b1, e1
+		}
+		if b1 > b2 {
+			return -1, b1, e1
+		}
+		return 1, b2, e2
+	}
+
+	if b1 > b2 && e1 > e2 {
+		return -1, b1, e1
+	}
+
+	if b2 > b1 && e2 > e1 {
+		return 1, b2, e2
+	}
+
+	// Select the higher exponent. call that b1, e1.
+	// Divide both powers by b1^e2
+	// Subtract e2 from e1
+
+	answer := 0
+	if e1 > e2 {
+		answer, _, _ = compare(b1, e1-e2, b2/b1, e2)
+	} else {
+		answer, _, _ = compare(b1/b2, e1, b2, e2-e1)
+	}
+	if answer == -1 {
+		return -1, b1, e1
+	}
+	if answer == 0 {
+		return 0, b1, e1
+	}
+	return 1, b2, e2
+
+}
+
+func readFile() [][2]int {
+	raw, _ := ioutil.ReadFile("p099_base_exp.txt")
+	lines := strings.Split(string(raw), "\n")
+
+	powers := make([][2]int, 0)
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		be := strings.Split(line, ",")
+		b, _ := strconv.Atoi(be[0])
+		e, _ := strconv.Atoi(be[1])
+		powers = append(powers, [2]int{b, e})
+	}
+
+	return powers
+}
+
+func max(powers [][2]int) int {
+	b1 := 0.0
+	e1 := 0
+	maxLine := 0
+	answer := 0
+
+	for i := 0; i < len(powers); i++ {
+		b2 := float64(powers[i][0])
+		e2 := powers[i][1]
+		answer, b1, e1 = compare(b1, e1, b2, e2)
+		if answer == 1 {
+			maxLine = i
+		}
+	}
+
+	// Our array was zero-based, but the answer is going
+	// to be 1-based. Add one to account for that.
+	maxLine++
+
+	return maxLine
+}
+
+func main() {
+	fmt.Println("Welcome to 099")
+
+	lines := readFile()
+	maxLine := max(lines)
+	fmt.Println("The max line is:", maxLine)
+}
---
+++ golang/099/099_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestS(t *testing.T) {
+	testCases := []struct {
+		b1       float64
+		e1       int
+		b2       float64
+		e2       int
+		expected int
+		bMax     float64
+		eMax     int
+	}{
+		// b1 == b2
+		{10, 5, 10, 5, 0, 10, 5},
+		{10, 6, 10, 5, -1, 10, 6},
+		{10, 5, 10, 7, 1, 10, 7},
+
+		// e1 == e2
+		{10, 5, 10, 5, 0, 10, 5},
+		{11, 5, 10, 5, -1, 11, 5},
+		{10, 5, 12, 5, 1, 12, 5},
+
+		// b1 > b2 && e1 > e2
+		{11, 6, 10, 5, -1, 11, 6},
+
+		// b2 > b1 && e2 > e1
+		{10, 5, 12, 7, 1, 12, 7},
+
+		// All other cases...
+		{3, 2, 2, 3, -1, 3, 2},
+		{5, 3, 12, 2, 1, 12, 2},
+	}
+
+	for _, testCase := range testCases {
+		answer, bMax, eMax := compare(testCase.b1, testCase.e1, testCase.b2, testCase.e2)
+		if answer != testCase.expected || bMax != testCase.bMax || eMax != testCase.eMax {
+			t.Errorf("ERROR: For %.2f^%d <=> %.2f^%d expected %d: %.2f^%d, got %d: %.2f^%d", testCase.b1, testCase.e1, testCase.b2, testCase.e2, testCase.expected, testCase.bMax, testCase.eMax, answer, bMax, eMax)
+		}
+	}
+}
+
+func TestMax(t *testing.T) {
+	testCases := []struct {
+		n        [][2]int
+		expected int
+	}{
+		{[][2]int{
+			{999, 999},
+			{888, 888},
+			{777, 777},
+		}, 1},
+		{[][2]int{
+			{888, 888},
+			{999, 999},
+			{777, 777},
+		}, 2},
+		{[][2]int{
+			{888, 888},
+			{777, 777},
+			{999, 999},
+		}, 3},
+	}
+
+	for _, testCase := range testCases {
+		answer := max(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %v expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
