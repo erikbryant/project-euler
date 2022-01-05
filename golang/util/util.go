1c78b3b540d15ee21f17db247078721c38988bd2
Erik Bryant <erikbryantology@gmail.com>
2022-01-05 15:32:08 -0700

    Add documentation

--- golang/util/util.go
+++ golang/util/util.go
@@ -35,6 +35,10 @@ func heapPermutation(digits []int, size int, c chan []int) {
 	}
 }
 
+// Generate all permutations of the first n digits.
+// For example:
+//   n=2 [1 2] [2 1]
+//   n=3 [1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1]
 func MakeDigits(n int, c chan []int) {
 	defer close(c)
 
