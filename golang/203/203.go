27a1f5727a0d9f02d750df56da8764b5febe0c59
Erik Bryant <erikbryantology@gmail.com>
2022-02-04 16:23:01 -0700

    Solution to Project Euler #203

---
+++ golang/203/203.go
@@ -0,0 +1,60 @@
+package main
+
+// go fmt && golint && go test && go run 203.go -cpuprofile cpu.prof && echo top | go tool pprof cpu.prof
+
+import (
+	"../util"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+)
+
+// Find the sum of the distinct squarefree numbers in the first 51 rows of
+// Pascal's triangle.
+
+// sumDistinctSquareFree returns the sum of the distinct squarefree numbers in
+// the given triangle
+func sumDistinctSquareFree(rows [][]int) int {
+	distinct := map[int]bool{}
+
+	for _, row := range rows {
+		for _, val := range row {
+			if util.SquareFree(val) {
+				distinct[val] = true
+			}
+		}
+	}
+
+	sum := 0
+	for key := range distinct {
+		sum += key
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Printf("Welcome to 203\n\n")
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	max := 51
+	triangle := util.PascalTriangle(max)
+	sum := sumDistinctSquareFree(triangle)
+
+	fmt.Println("Sum of the first", max, "rows of distinct squarefree numbers:", sum)
+}
