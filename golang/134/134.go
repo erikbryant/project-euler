42c19ed97c0e5df481fb376594acef21c1982b8c
Erik Bryant <erikbryantology@gmail.com>
2022-02-08 11:18:46 -0700

    Solution to Project Euler #134

---
+++ golang/134/134.go
@@ -0,0 +1,99 @@
+package main
+
+// go fmt && golint && go test && go run 134.go -cpuprofile cpu.prof && echo top | go tool pprof cpu.prof
+
+import (
+	"../primes"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"runtime/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+)
+
+// Consider the consecutive primes p1 = 19 and p2 = 23. It can be verified that
+// 1219 is the smallest number such that the last digits are formed by p1 whilst
+// also being divisible by p2.
+//
+// In fact, with the exception of p1 = 3 and p2 = 5, for every pair of
+// consecutive primes, p2 > p1, there exist values of n for which the last
+// digits are formed by p1 and n is divisible by p2. Let S be the smallest of
+// these values of n.
+//
+// Find ∑ S for every pair of consecutive primes with 5 ≤ p1 ≤ 1,000,000.
+
+// init loads the prime tables
+func init() {
+	primes.Load("../primes.gob")
+}
+
+// LCM returns the lowest common multiple of a and b where the digits in LCM
+// end with the digits in a
+func LCM(a, b int) int {
+	// We start with NNN where NNN are the digits in a. We add 1000 to it on each
+	// iteration, resulting in 1NNN, 2NNN, 3NNN, etc. When we add the 1000 mask we
+	// need to also step up bMult correspondingly, so calculate a bStep that is
+	// close to, but less than, 1000.
+	lenSuffix := math.Trunc(math.Log10(float64(a))) + 1.0
+	mask := int(math.Pow(10.0, lenSuffix))
+	bStep := b * (mask / b)
+
+	candidate := a
+	bMult := 0
+
+	for {
+		candidate += mask
+		bMult += bStep
+		for ; bMult < candidate; bMult += b {
+			// Adding bStep gets us close to candidate.
+			// Make sure we get all the way there.
+		}
+		if candidate == bMult {
+			break
+		}
+	}
+
+	return candidate
+}
+
+func looper(maxP int) int {
+	sum := 0
+
+	// fmt.Printf("     p1    p2            LCM            sum\n")
+
+	for i := 0; primes.PackedPrimes[i] <= maxP; i++ {
+		p1 := primes.PackedPrimes[i]
+		if p1 < 5 {
+			continue
+		}
+		p2 := primes.PackedPrimes[i+1]
+		lcm := LCM(p1, p2)
+		sum += lcm
+		// fmt.Printf("%6d %6d %14d %14d\n", p1, p2, lcm, sum)
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Printf("Welcome to 134\n\n")
+
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	maxP := 1000 * 1000
+	sum := looper(maxP)
+	fmt.Println("For 5 <= p <=", maxP, "∑ S =", sum)
+}
---
+++ golang/134/134_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLCM(t *testing.T) {
+	testCases := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{5, 7, 35},
+		{7, 11, 77},
+		{11, 13, 611},
+		{13, 17, 1513},
+		{19, 23, 1219},
+	}
+
+	for _, testCase := range testCases {
+		answer := LCM(testCase.a, testCase.b)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d %d expected %d, got %d", testCase.a, testCase.b, testCase.expected, answer)
+		}
+	}
+}
+
+func TestLooper(t *testing.T) {
+	testCases := []struct {
+		n        int
+		expected int
+	}{
+		{0, 0},
+		{5, 35},
+		{7, 112},
+		{11, 723},
+		{13, 2236},
+		{17, 3053},
+		{19, 4272},
+	}
+
+	for _, testCase := range testCases {
+		answer := looper(testCase.n)
+		if answer != testCase.expected {
+			t.Errorf("ERROR: For %d expected %d, got %d", testCase.n, testCase.expected, answer)
+		}
+	}
+}
