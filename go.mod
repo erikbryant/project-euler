317c411ce6db00b7a2d72a34646bc5a7e388e762
Erik Bryant <erikbryantology@gmail.com>
2024-01-06 12:21:19 -0500

    Factor useful functions out to util-golang repo

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/erikbryant/project-euler
 
 go 1.21.5
 
-require github.com/erikbryant/util-golang v0.0.3-0.20240106050958-06529818785e
+require github.com/erikbryant/util-golang v0.0.6
--- golang/045/045.go
+++ golang/045/045.go
@@ -2,28 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	"github.com/erikbryant/util-golang/figurate"
 )
-
-func isInt(n float64) bool {
-	return n > 0 && n == float64(int(n))
-}
-
-func pentagonal(n int) bool {
-	root := math.Sqrt(float64(24*n + 1))
-	return isInt((1+root)/6) || isInt((1-root)/6)
-}
-
-func hexagonal(n int) bool {
-	root := math.Sqrt(float64(8*n + 1))
-	return isInt((1+root)/4) || isInt((1-root)/4)
-}
 
 func main() {
 	n := 1
 	for {
 		triangle := (n*n + n) / 2
-		if pentagonal(triangle) && hexagonal(triangle) {
+		if figurate.IsPentagonal(triangle) && figurate.IsHexagonal(triangle) {
 			fmt.Println(n, " (", triangle, ") is Tri + Pent + Hex")
 			if triangle > 40755 {
 				break
