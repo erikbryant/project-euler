a602430917fdf48d6e007847674b9cc482f63f46
Erik Bryant <erikbryantology@gmail.com>
2025-09-17 00:54:15 +0100

    Easier to select perf/time

--- golang/templates/nnn.go
+++ golang/templates/nnn.go
@@ -1,31 +1,12 @@
 package main
 
-// go fmt ./... && go vet ./... && go test && go build nnn.go && ./nnn -cpuprofile cpu.prof && echo top | go tool pprof cpu.prof
+// go fmt ./... && go vet ./... && go test && go build nnn.go && time ./nnn
 
 import (
 	// "github.com/erikbryant/util-golang/primes"
-	"flag"
 	"fmt"
-	"log"
-	"os"
-	"runtime/pprof"
-)
-
-var (
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 )
 
 func main() {
 	fmt.Printf("Welcome to nnn\n\n")
-
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
-
 }
