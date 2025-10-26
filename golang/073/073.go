9587fc93f5eaec385af789c03cc93992be9a665d
Erik Bryant <erikbryantology@gmail.com>
2025-10-26 10:42:33 -0700

    Code cleanup

--- golang/073/073.go
+++ golang/073/073.go
@@ -1,10 +1,24 @@
 package main
 
+// go fmt ./... && go vet ./... && go test ./... && go build 073.go && time ./073
+
 import (
 	"fmt"
 
 	"github.com/erikbryant/util-golang/primes"
 )
+
+// Consider the fraction, n/d, where n and d are positive integers. If n < d and HCF(n, d)=1,
+// it is called a reduced proper fraction.
+//
+// If we list the set of reduced proper fractions for d <= 8 in ascending order of size, we get:
+// 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, _3/8_, _2/5_, _3/7_, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7,
+// 3/4, 4/5, 5/6, 6/7, 7/8
+//
+// It can be seen that there are 3 fractions between 1/3 and 1/2.
+//
+// How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for
+// d <= 12,000?
 
 type fraction struct {
 	n int
@@ -13,8 +27,8 @@ type fraction struct {
 
 // Reduce a fraction n/d such that the HCF(n, d) == 1.
 func reduce(n, d int) (rN, rD int) {
-	for i := 0; primes.Primes[i] <= n; i++ {
-		p := primes.Primes[i]
+	for i := 0; int(primes.Primes[i]) <= n; i++ {
+		p := int(primes.Primes[i])
 		for n%p == 0 && d%p == 0 {
 			n = n / p
 			d = d / p
@@ -49,5 +63,7 @@ func rpf(max int) int {
 }
 
 func main() {
-	fmt.Println("# fractions:", rpf(12000))
+	fmt.Printf("Welcome to 073\n\n")
+
+	fmt.Printf("# fractions between 1/3 and 1/2 for d <= 12,000: %d\n\n", rpf(12000))
 }
---
+++ golang/073/go.mod
@@ -0,0 +1,5 @@
+module 073
+
+go 1.25.3
+
+require github.com/erikbryant/util-golang v0.21.0
