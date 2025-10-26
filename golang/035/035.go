95b58c7426d4abdfc5ef43f0020a6e4676b0f274
Erik Bryant <erikbryantology@gmail.com>
2025-10-26 09:28:55 -0700

    Code cleanup

--- golang/035/035.go
+++ golang/035/035.go
@@ -1,11 +1,20 @@
 package main
 
+// go fmt ./... && go vet ./... && go test ./... && go build 035.go && time ./035
+
 import (
 	"fmt"
 	"strconv"
 
 	"github.com/erikbryant/util-golang/primes"
 )
+
+// The number, 197, is called a circular prime because all rotations of the
+// digits: 197, 971, and 719, are themselves prime.
+//
+// There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+//
+// How many circular primes are there below one million?
 
 func rotate(s string) string {
 	return s[1:] + string(s[0])
@@ -28,12 +37,15 @@ func circular(number int) bool {
 }
 
 func main() {
+	fmt.Printf("Welcome to 035\n\n")
+
 	circularCount := 0
-	for _, prime := range primes.Primes {
+	for _, prime := range primes.Iter() {
 		if circular(prime) {
 			fmt.Println("Circular: ", prime)
 			circularCount++
 		}
 	}
-	fmt.Println("Circular count: ", circularCount)
+
+	fmt.Printf("\nCircular prime count < 1,000,000: %d\n\n", circularCount)
 }
---
+++ golang/035/go.mod
@@ -0,0 +1,5 @@
+module 035
+
+go 1.25.3
+
+require github.com/erikbryant/util-golang v0.21.0
