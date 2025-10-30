4e8f9376671210df45ab3a574aeda5607b44e47f
Erik Bryant <erikbryantology@gmail.com>
2025-10-29 20:15:42 -0700

    Upgrade to primey

--- golang/357/357.go
+++ golang/357/357.go
@@ -1,11 +1,19 @@
 package main
 
+// go fmt ./... && go vet ./... && go test ./... && go build 357.go && time ./357
+
 import (
 	"fmt"
 	"math"
 
-	"github.com/erikbryant/util-golang/primes"
+	"github.com/erikbryant/util-golang/primey"
 )
+
+// Consider the divisors of 30: 1,2,3,5,6,10,15,30.
+// It can be seen that for every divisor d of 30, d + 30 / d is prime.
+//
+// Find the sum of all positive integers n not exceeding 100,000,000
+// such that for every divisor d of n, d + n / d is prime.
 
 func primeDivisors(n int) bool {
 	//
@@ -20,7 +28,7 @@ func primeDivisors(n int) bool {
 	root := int(math.Sqrt(float64(n)))
 	for d := 1; d <= root; d++ {
 		if n%d == 0 {
-			if !primes.Prime(d + n/d) {
+			if !primey.Prime(d + n/d) {
 				return false
 			}
 		}
@@ -30,6 +38,8 @@ func primeDivisors(n int) bool {
 }
 
 func main() {
+	fmt.Printf("Welcome to 357\n\n")
+
 	sum := 0
 	for n := 1; n <= 1000*1000*100; n++ {
 		if primeDivisors(n) {
@@ -37,5 +47,5 @@ func main() {
 		}
 	}
 
-	fmt.Println("Sum: ", sum)
+	fmt.Printf("Sum of n where n <= 100,000,000 and f(n) is prime: %d\n\n", sum)
 }
