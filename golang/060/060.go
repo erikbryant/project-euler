e9361ad517d07fc84c5f815f7f25f3b0a6e0c70c
Erik Bryant <erikbryantology@gmail.com>
2025-10-29 22:41:39 -0700

    Upgrade to primey

--- golang/060/060.go
+++ golang/060/060.go
@@ -7,7 +7,7 @@ import (
 	"math"
 	"strconv"
 
-	"github.com/erikbryant/util-golang/primes"
+	"github.com/erikbryant/util-golang/primey"
 )
 
 // The primes 3, 7, 109, and 673, are quite remarkable. By taking any two primes and
@@ -24,19 +24,18 @@ var (
 )
 
 func init() {
-	for _, prime := range primes.Iter() {
+	for _, prime := range primey.Iter() {
 		s := strconv.Itoa(prime)
 		powPrimes = append(powPrimes, int(math.Pow10(len(s))))
 	}
-
-	maxPrime = int(primes.Primes[len(primes.Primes)-1])
+	maxPrime = primey.PrimeMax()
 }
 
 func allCombosPrime(p []int) bool {
 	for i := 0; i < len(p); i++ {
 		for j := i + 1; j < len(p); j++ {
-			p1 := int(primes.Primes[p[i]])
-			p2 := int(primes.Primes[p[j]])
+			p1 := primey.Nth(p[i])
+			p2 := primey.Nth(p[j])
 			p1Len := powPrimes[p[i]]
 			p2Len := powPrimes[p[j]]
 			c1 := p1*p2Len + p2
@@ -44,7 +43,7 @@ func allCombosPrime(p []int) bool {
 			if c1 > maxPrime || c2 > maxPrime {
 				return false
 			}
-			if !(primes.Prime(c1) && primes.Prime(c2)) {
+			if !(primey.Prime(c1) && primey.Prime(c2)) {
 				return false
 			}
 		}
@@ -56,29 +55,29 @@ func printPrimes(p []int) int {
 	sum := 0
 
 	for i := 0; i < len(p); i++ {
-		sum += int(primes.Primes[p[i]])
+		sum += primey.Nth(p[i])
 	}
 
 	if len(p) == 4 {
-		fmt.Println("Sum:", sum, p, []int32{primes.Primes[p[0]], primes.Primes[p[1]], primes.Primes[p[2]], primes.Primes[p[3]]})
+		fmt.Println("Sum:", sum, p, []int{primey.Nth(p[0]), primey.Nth(p[1]), primey.Nth(p[2]), primey.Nth(p[3])})
 	}
 
 	if len(p) == 5 {
-		fmt.Println("Sum:", sum, p, []int32{primes.Primes[p[0]], primes.Primes[p[1]], primes.Primes[p[2]], primes.Primes[p[3]], primes.Primes[p[4]]})
+		fmt.Println("Sum:", sum, p, []int{primey.Nth(p[0]), primey.Nth(p[1]), primey.Nth(p[2]), primey.Nth(p[3]), primey.Nth(p[4])})
 	}
 
 	return sum
 }
 
 func findPrimes(maxP int) {
-	for a := 0; int(primes.Primes[a]) <= maxP; a++ {
-		for b := a + 1; int(primes.Primes[b]) <= maxP; b++ {
+	for a := 0; primey.Nth(a) <= maxP; a++ {
+		for b := a + 1; primey.Nth(b) <= maxP; b++ {
 			if allCombosPrime([]int{a, b}) {
-				for c := b + 1; int(primes.Primes[c]) <= maxP; c++ {
+				for c := b + 1; primey.Nth(c) <= maxP; c++ {
 					if allCombosPrime([]int{a, b, c}) {
-						for d := c + 1; int(primes.Primes[d]) <= maxP; d++ {
+						for d := c + 1; primey.Nth(d) <= maxP; d++ {
 							if allCombosPrime([]int{a, b, c, d}) {
-								for e := 1; int(primes.Primes[e]) <= maxP; e++ {
+								for e := 1; primey.Nth(e) <= maxP; e++ {
 									p := []int{a, b, c, d, e}
 									if allCombosPrime(p) {
 										printPrimes(p)
--- golang/060/060_test.go
+++ golang/060/060_test.go
@@ -3,17 +3,17 @@ package main
 import (
 	"testing"
 
-	"github.com/erikbryant/util-golang/primes"
+	"github.com/erikbryant/util-golang/primey"
 )
 
 func TestAllCombosPrime(t *testing.T) {
 	testCases := []struct {
 		primeIndexes []int
-		primes       []int32
+		primes       []int
 		expected     bool
 	}{
-		{[]int{0, 1, 2, 3}, []int32{2, 3, 5, 7}, false},
-		{[]int{1, 3, 28, 121}, []int32{3, 7, 109, 673}, true},
+		{[]int{0, 1, 2, 3}, []int{2, 3, 5, 7}, false},
+		{[]int{1, 3, 28, 121}, []int{3, 7, 109, 673}, true},
 	}
 
 	// allCombosPrime() takes the index of the primes, not the
@@ -21,8 +21,8 @@ func TestAllCombosPrime(t *testing.T) {
 	// Make sure we got the indexes right. :-)
 	for _, testCase := range testCases {
 		for i := 0; i < len(testCase.primeIndexes); i++ {
-			if testCase.primes[i] != primes.Primes[testCase.primeIndexes[i]] {
-				t.Errorf("ERROR: For %v, %v expected %d, got %d", testCase.primeIndexes, testCase.primes, testCase.primes[i], primes.Primes[testCase.primeIndexes[i]])
+			if testCase.primes[i] != primey.Nth(testCase.primeIndexes[i]) {
+				t.Errorf("ERROR: For %v, %v expected %d, got %d", testCase.primeIndexes, testCase.primes, testCase.primes[i], primey.Nth(testCase.primeIndexes[i]))
 			}
 		}
 	}
